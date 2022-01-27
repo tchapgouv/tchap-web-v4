42afbeee2a81a1fbdd46fe6414e4fc312a41fe53
Calev Eliacheff <c.eliacheff@gmail.com>
2022-01-27 12:35:49 +0100

    Link to forked repo + theme

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,14 @@ const cssThemes = {
     // CSS themes
     "theme-legacy-light": "./node_modules/matrix-react-sdk/res/themes/legacy-light/css/legacy-light.scss",
     "theme-legacy-dark": "./node_modules/matrix-react-sdk/res/themes/legacy-dark/css/legacy-dark.scss",
-    "theme-light": "./node_modules/matrix-react-sdk/res/themes/light/css/light.scss",
+    // "theme-light": "./node_modules/matrix-react-sdk/res/themes/light/css/light.scss",
+    "theme-light": "./node_modules/matrix-react-sdk/res/themes/tchap/css/tchap.scss",
     "theme-light-high-contrast":
         "./node_modules/matrix-react-sdk/res/themes/light-high-contrast/css/light-high-contrast.scss",
     "theme-dark": "./node_modules/matrix-react-sdk/res/themes/dark/css/dark.scss",
     "theme-light-custom": "./node_modules/matrix-react-sdk/res/themes/light-custom/css/light-custom.scss",
     "theme-dark-custom": "./node_modules/matrix-react-sdk/res/themes/dark-custom/css/dark-custom.scss",
+    // "theme-tchap": "./node_modules/matrix-react-sdk/res/themes/tchap/css/tchap.scss",
 };
 
 function getActiveThemes() {
