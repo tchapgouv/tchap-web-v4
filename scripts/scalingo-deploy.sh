b2a67fde2f6c673f91a1cc76b2cdd06483c9e36c
Calev Eliacheff <c.eliacheff@gmail.com>
2022-03-23 14:24:35 +0100

    Update script

--- scripts/scalingo-deploy.sh
+++ scripts/scalingo-deploy.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-version=$(grep '"version"' package.json | cut -d '"' -f 4 | head -n 1)
+# only available in npm versions > 16
+version=$(npm pkg get version)
+#version=$(grep '"version"' package.json | cut -d '"' -f 4 | head -n 1)
 
 yarn clean
 VERSION=$version yarn build
@@ -14,7 +16,7 @@ cp nginx.conf.erb webapp/nginx.conf.erb
 mkdir -p dist
 cp -r webapp element-$version
 
-"$(dirname "$0")"/normalize-version.sh "${version}" > "element-$version/version"
+"$(dirname $0)"/normalize-version.sh "${version}" > "element-$version/version"
 
 tar chvzf "dist/element-$version.tar.gz" "element-$version"
 rm -r "element-$version"
@@ -22,4 +24,4 @@ rm -r "element-$version"
 echo
 echo "Packaged dist/element-$version.tar.gz"
 
-#scalingo --app tchap-dev --region osc-secnum-fr1 deploy "dist/element-$version.tar.gz"
+scalingo --app tchap-dev --region osc-secnum-fr1 deploy "dist/element-$version.tar.gz"
