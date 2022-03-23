baac6eedc9f19cb8b8a375ee4c2a430915556e32
Estelle Comment <estelle.comment@gmail.com>
2022-03-23 17:48:31 +0100

    Remove extra space in a translation

--- src/components/views/dialogs/TchapServerPickerDialog.tsx
+++ src/components/views/dialogs/TchapServerPickerDialog.tsx
@@ -105,7 +105,7 @@ export default class TchapServerPickerDialog extends React.PureComponent<IProps,
         >
             <form>
                 <div>
-                    <label htmlFor="homeservers"> { _t("Choose a homeserver :") }</label>
+                    <label htmlFor="homeservers"> { _t("Choose a homeserver:") }</label>
                 </div>
                 <div>
                     <StyledRadioGroup
