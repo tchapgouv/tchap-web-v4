65b669ddf72918a90b8ae95ddb37cdaf5beb1f03
Estelle Comment <estelle.comment@gmail.com>
2022-04-25 16:42:15 +0200

    Refactor test, much pretty, wow

--- test/unit-tests/lib/createTchapRoom-test.ts
+++ test/unit-tests/lib/createTchapRoom-test.ts
@@ -7,90 +7,83 @@ describe("Create room options", () => {
     });
 
     it("builds option for private room", (done) => {
-        expect(roomCreateOptions("testName", TchapRoomType.Private)).toStrictEqual(
-            {
-                createOpts: {
-                  name: "testName",
-                  creation_content: {
+        const privateRoomExpectedOpts = {
+            createOpts: {
+                name: "testName",
+                creation_content: {
                     "m.federate": true,
-                  },
-                  accessRule: "restricted",
-                  visibility: "private",
-                  preset: "private_chat",
                 },
-                guestAccess: false,
-                joinRule: "invite",
-                encryption: true,
-                historyVisibility: "joined",
-              }
-        )
-        done()
+                accessRule: "restricted",
+                visibility: "private",
+                preset: "private_chat",
+            },
+            guestAccess: false,
+            joinRule: "invite",
+            encryption: true,
+            historyVisibility: "joined",
+        };
+        expect(roomCreateOptions("testName", TchapRoomType.Private)).toStrictEqual(privateRoomExpectedOpts);
+        done();
     });
 
     it("builds option for public room without federation", (done) => {
-        expect(roomCreateOptions("testName", TchapRoomType.Forum, false)).toStrictEqual(
-            {
-                createOpts: {
-                  name: "testName",
-                  creation_content: {
+        const publicRoomWithoutFederationExpectedOpts = {
+            createOpts: {
+                name: "testName",
+                creation_content: {
                     "m.federate": false,
-                  },
-                  accessRule: "restricted",
-                  visibility: "public",
-                  preset: "public_chat",
                 },
-                guestAccess: false,
-                joinRule: "public",
-                encryption: false,
-                historyVisibility: "shared",
-              }
-        )
-        done()
-
+                accessRule: "restricted",
+                visibility: "public",
+                preset: "public_chat",
+            },
+            guestAccess: false,
+            joinRule: "public",
+            encryption: false,
+            historyVisibility: "shared",
+        };
+        expect(roomCreateOptions("testName", TchapRoomType.Forum, false)).toStrictEqual(publicRoomWithoutFederationExpectedOpts);
+        done();
     });
 
     it("builds option for public room with federation", (done) => {
-        expect(roomCreateOptions("testName", TchapRoomType.Forum, true)).toStrictEqual(
-            {
-                createOpts: {
-                  name: "testName",
-                  creation_content: {
+        const publicRoomWithFederationExpectedOpts = {
+            createOpts: {
+                name: "testName",
+                creation_content: {
                     "m.federate": true,
-                  },
-                  accessRule: "restricted",
-                  visibility: "public",
-                  preset: "public_chat",
                 },
-                guestAccess: false,
-                joinRule: "public",
-                encryption: false,
-                historyVisibility: "shared",
-              }
-        )
-        done()
-
+                accessRule: "restricted",
+                visibility: "public",
+                preset: "public_chat",
+            },
+            guestAccess: false,
+            joinRule: "public",
+            encryption: false,
+            historyVisibility: "shared",
+        };
+        expect(roomCreateOptions("testName", TchapRoomType.Forum, true)).toStrictEqual(publicRoomWithFederationExpectedOpts);
+        done();
     });
 
     it("builds option for external room", (done) => {
-        expect(roomCreateOptions("testName", TchapRoomType.External)).toStrictEqual(
-            {
-                createOpts: {
-                  name: "testName",
-                  creation_content: {
+        const externalRoomExpectedOpts = {
+            createOpts: {
+                name: "testName",
+                creation_content: {
                     "m.federate": true,
-                  },
-                  accessRule: "unrestricted",
-                  visibility: "private",
-                  preset: "private_chat",
                 },
-                guestAccess: false,
-                joinRule: "invite",
-                encryption: true,
-                historyVisibility: "joined",
-              }
-        )
-        done()
-
+                accessRule: "unrestricted",
+                visibility: "private",
+                preset: "private_chat",
+            },
+            guestAccess: false,
+            joinRule: "invite",
+            encryption: true,
+            historyVisibility: "joined",
+        };
+        expect(roomCreateOptions("testName", TchapRoomType.External)).toStrictEqual(externalRoomExpectedOpts);
+        done();
     });
 
     it("handles wrong inputs", (done) => {
