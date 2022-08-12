d7032d6fb44969479aa634bee31edf01df69bf97
Michael Weimann <mail@michael-weimann.eu>
2022-08-12 12:19:51 +0200

    Add MCS api

---
+++ src/content-scanner/ContentScanner.ts
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2022 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { getHttpUriForMxc } from "matrix-js-sdk/src/content-repo";
+import { IEncryptedFile } from "matrix-react-sdk/src/customisations/models/IMediaEventContent";
+import { PkEncryption } from "@matrix-org/olm";
+import { ResizeMethod } from "matrix-js-sdk/src/@types/partials";
+import { MatrixClientPeg } from "matrix-react-sdk/src/MatrixClientPeg";
+
+import { ContentScannerInterface } from "./ContentScannerInterface";
+import { NoopContentScanner } from "./NoopContentScanner";
+import { readScannerUrlConfig } from "./readScannerUrlConfig";
+
+export enum ScanErrorReason {
+    RequestFailed = "MCS_MEDIA_REQUEST_FAILED",
+    DecryptFailed = "MCS_MEDIA_FAILED_TO_DECRYPT",
+    NotClean = "MCS_MEDIA_NOT_CLEAN",
+    BadDecryption = "MCS_BAD_DECRYPTION",
+    Malformed = "MCS_MALFORMED_JSON",
+}
+
+export interface ScanError {
+    info: string;
+    reason: ScanErrorReason;
+}
+
+export interface ScanResult {
+    clean: boolean;
+    scanned: boolean;
+    info: string;
+}
+
+/**
+ * Content scanner implementation that interacts with a Matrix Content Scanner.
+ * @see https://github.com/matrix-org/matrix-content-scanner
+ */
+export class ContentScanner implements ContentScannerInterface {
+    private static internalInstance: ContentScanner;
+
+    private mcsKey: PkEncryption = new global.Olm.PkEncryption();
+    private keySet = false;
+    private pendingScans = new Map<string, Promise<boolean>>();
+
+    constructor(private scannerUrl: string) {
+    }
+
+    public urlForMxc(mxc: string, width?: number, height?: number, method?: ResizeMethod): string {
+        const matrixUrl = getHttpUriForMxc(this.scannerUrl, mxc, width, height, method);
+        return matrixUrl.replace(/media\/r0/, "media_proxy/unstable");
+    }
+
+    public async download(mxc: string, file?: IEncryptedFile): Promise<Response> {
+        if (!file) {
+            return fetch(this.urlForMxc(mxc));
+        }
+
+        if (!this.keySet) {
+            const k = await fetch(this.scannerUrl + "/_matrix/media_proxy/unstable/public_key").then(r => r.json());
+            this.mcsKey.set_recipient_key(k['public_key']);
+            this.keySet = true;
+        }
+
+        return fetch(this.scannerUrl + "/_matrix/media_proxy/unstable/download_encrypted", {
+            method: "POST",
+            body: JSON.stringify({
+                encrypted_body: this.mcsKey.encrypt(JSON.stringify({ file })),
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
+
+    public async scan(mxc: string, file?: IEncryptedFile): Promise<boolean> {
+        // XXX: we're assuming that encryption won't be a differentiating factor and that the MXC URIs
+        // will be different.
+        if (this.pendingScans.has(mxc)) {
+            return this.pendingScans.get(mxc);
+        }
+
+        // eslint-disable-next-line no-async-promise-executor
+        const promise = new Promise<boolean>(async resolve => {
+            let response: Response;
+
+            if (file) {
+                if (!this.keySet) {
+                    const k = await fetch(this.scannerUrl + "/_matrix/media_proxy/unstable/public_key")
+                        .then(r => r.json());
+                    this.mcsKey.set_recipient_key(k["public_key"]);
+                    this.keySet = true;
+                }
+
+                response = await fetch(this.scannerUrl + "/_matrix/media_proxy/unstable/scan_encrypted", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        encrypted_body: this.mcsKey.encrypt(JSON.stringify({ file })),
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+            } else {
+                const url = this.scannerUrl + `/_matrix/media_proxy/unstable/scan/${mxc.substring('mxc://'.length)}`;
+                response = await fetch(url);
+            }
+
+            const responseJson: ScanResult = await response.json();
+            resolve(responseJson.clean);
+        });
+        this.pendingScans.set(mxc, promise);
+        return promise;
+    }
+
+    public static get instance(): ContentScannerInterface {
+        if (!ContentScanner.internalInstance) {
+            const scannerUrl = readScannerUrlConfig();
+
+            if (scannerUrl) {
+                ContentScanner.internalInstance = new ContentScanner(scannerUrl);
+            } else {
+                ContentScanner.internalInstance = new NoopContentScanner(MatrixClientPeg.get());
+            }
+        }
+
+        return ContentScanner.internalInstance;
+    }
+}
