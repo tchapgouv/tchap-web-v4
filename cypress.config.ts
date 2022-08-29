1e30f0b667d74a21179975c2e698487803d7570c
Calev Eliacheff <c.eliacheff@gmail.com>
2022-08-29 16:25:01 +0200

    Add informations on cypress files

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// TCHAP: copied from matrix-rect-sdk
 import { defineConfig } from 'cypress';
 import 'dotenv/config';
 
---
+++ cypress/README.md
@@ -0,0 +1,1 @@
+This folder is copied from the matrix-react-sdk and contains Tchap's specifics e2e tests
