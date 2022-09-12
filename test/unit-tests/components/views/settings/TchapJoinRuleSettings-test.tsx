45a0f98044418b9f636be3eaaf46b87b09f88ff2
olivier <odelcroi@gmail.com>
2022-09-12 15:38:08 +0200

    update test

--- test/unit-tests/components/views/settings/TchapJoinRuleSettings-test.tsx
+++ test/unit-tests/components/views/settings/TchapJoinRuleSettings-test.tsx
@@ -4,21 +4,33 @@ import { render, screen } from "@testing-library/react";
 import TchapJoinRuleSettings  from "../../../../../src/components/views/settings/TchapJoinRuleSettings";
 import { createTestClient, mkStubRoom } from "matrix-react-sdk/test/test-utils/test-utils";
 
+
 describe("TchapJoinRule", () => {
 
+    //build stub props
+    const props = {
+        room: mkStubRoom("roomId", "roomName", createTestClient()),
+        closeSettingsFn(){},
+        onError(error: Error){},
+    }
+
     beforeEach(() => {
+        
+    });
 
-        const props = {
-            room: mkStubRoom("roomId", "roomName", createTestClient()),
-            closeSettingsFn(){},
-            onError(error: Error){}
-        }
 
+    it("should render the tchap join rule without spaces option", () => {
+        //arrange
         render(<TchapJoinRuleSettings {...props} />);
+
+        //assert that spaces option is not here while private and public are
+        const privateText = "Private (invite only)"
+        const publicText = "Public"
+        const spaceText = "Anyone in a space can find and join"
+        
+        expect(screen.getByText(privateText)).toBeDefined();
+        expect(screen.getByText(publicText)).toBeDefined();
+        expect(screen.queryByText(spaceText)).toBe(null);
     });
 
-    it("should render the tchap join rule without spaces", () => {
-        //just testing 
-        //screen.getByText(text);
-    });
 });
