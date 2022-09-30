7dc649ee00c620c43eebb281e89c9c5c33b56446
Estelle Comment <estelle.comment@gmail.com>
2022-09-30 14:33:10 +0200

    Check the patch file is valid before doing anything

--- scripts/makePatch.ts
+++ scripts/makePatch.ts
@@ -6,14 +6,40 @@ if (process.argv.length !== 3) {
   console.log("my-patch-name should be listed in patches/patches.json");
   console.log("Known patch names :", Object.keys(patchFile));
   console.log("Aborting.\n");
-  return
+  process.exit(1);
 }
+
+const checkPatchFile = (patchFile) => {
+  console.log("Checking patch file is valid...");
+  const findFirstDuplicate = (arr) => {
+    let sorted_arr = arr.slice().sort();
+    for (let i = 0; i < sorted_arr.length - 1; i++) {
+      if (sorted_arr[i + 1] == sorted_arr[i]) {
+        return sorted_arr[i];
+      }
+    }
+    return;
+  }
+
+  let fileList = []
+  Object.values(patchFile).forEach(patchInfo => {
+    fileList = fileList.concat(patchInfo.files);
+  });
+  const dup = findFirstDuplicate(fileList);
+  if (dup) {
+    console.error("This file is used in two different patches:", dup);
+    console.error("The changes of the two packages will be mixed up. Don't do this.");
+    process.exit(1);
+  }
+  console.log("... patch file is valid.");
+};
+checkPatchFile(patchFile);
 
 const patchName = process.argv[2];
 const patchInfo = patchFile[patchName];
 if (!patchInfo) {
   console.log("Your patch name is not listed in patches/patches.json. Aborting.");
-  return
+  process.exit(1);
 }
 
 const fileList = patchInfo.files.join('|');
