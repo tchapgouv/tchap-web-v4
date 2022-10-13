8a6be7158c85b442e024382eb31d1d9ac7e80fc1
olivier <odelcroi@gmail.com>
2022-10-13 12:27:19 +0200

    add unit tests

--- src/util/TchapRoomUtils.ts
+++ src/util/TchapRoomUtils.ts
@@ -24,6 +24,7 @@ export default class TchapRoomUtils {
         if (tchapRoomAccessRule === TchapRoomAccessRule.Unrestricted) {
             return TchapRoomType.External;
         }
+        return TchapRoomType.Unknown;
     }
 
     /**
