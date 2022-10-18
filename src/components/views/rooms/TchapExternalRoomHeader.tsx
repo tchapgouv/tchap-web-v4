80d861a2be804be867e76f02dbb966c3985c5fe3
olivier <odelcroi@gmail.com>
2022-10-18 16:24:02 +0200

    add room header when externals are allowed

---
+++ src/components/views/rooms/TchapExternalRoomHeader.tsx
@@ -0,0 +1,51 @@
+import { MatrixEvent, Room, RoomStateEvent } from 'matrix-js-sdk/src/matrix';
+import { useTypedEventEmitter } from 'matrix-react-sdk/src/hooks/useEventEmitter';
+import React, { useEffect, useState } from 'react';
+
+import { TchapRoomAccessRule, TchapRoomAccessRulesEventId } from '../../../@types/tchap';
+import TchapRoomUtils from '../../../util/TchapRoomUtils';
+
+//import "../../../../res/css/views/rooms/_TchapRoomHeader.pcss";
+
+export interface IProps {
+    room?: Room;
+}
+
+export function checkExternalAllowed(room: Room): boolean {
+    return TchapRoomUtils.getTchapRoomAccessRule(room) === TchapRoomAccessRule.Unrestricted;
+}
+
+//define a function to manage internal state
+//inspired by RoomTopic.tsx
+function useExternalAllowed(room: Room): boolean {
+    //define the hook functions and initialize the state
+    const [isExternalAllowed, setExternalAllowed] = useState(checkExternalAllowed(room));
+
+    //listen to events to refresh the value if needed 
+    useTypedEventEmitter(room.currentState, RoomStateEvent.Events, (ev: MatrixEvent) => {
+        console.log(ev.getType());
+        if (ev.getType() === TchapRoomAccessRulesEventId) {
+            setExternalAllowed(checkExternalAllowed(room));
+        }
+    });
+    //refresh the state after rendering
+    useEffect(() => {
+        setExternalAllowed(checkExternalAllowed(room));
+    }, [room]);// Only re-run the effect if room changes
+
+    return isExternalAllowed;
+}
+
+export default function TchapExternalRoomHeader({
+    room,
+}: IProps) {
+    const isExternalAllowed = useExternalAllowed(room);
+
+    return (
+        isExternalAllowed ?
+            <div className="tc_RoomHeader_external">
+                <span>ouvert aux externes</span>
+            </div>
+            : null);
+}
+
