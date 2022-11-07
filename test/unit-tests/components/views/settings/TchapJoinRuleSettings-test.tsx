f080db3eab5c3afd12dbab9221a71e3f88f27593
Estelle Comment <estelle.comment@gmail.com>
2022-11-07 17:53:25 +0100

    fix linter

--- test/unit-tests/components/views/settings/TchapJoinRuleSettings-test.tsx
+++ test/unit-tests/components/views/settings/TchapJoinRuleSettings-test.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { mocked } from 'jest-mock';
-
-import TchapJoinRuleSettings  from "../../../../../src/components/views/settings/TchapJoinRuleSettings";
-import { createTestClient, mkStubRoom, mockStateEventImplementation, mkEvent } from "matrix-react-sdk/test/test-utils/test-utils";
+import { createTestClient, mkStubRoom, mockStateEventImplementation, mkEvent }
+    from "matrix-react-sdk/test/test-utils/test-utils";
 import { JoinRule, MatrixClient, Room } from "matrix-js-sdk/src/matrix";
+
+import TchapJoinRuleSettings from "../../../../../src/components/views/settings/TchapJoinRuleSettings";
 import { TchapRoomAccessRule, TchapRoomAccessRulesEventId } from "../../../../../src/@types/tchap";
 
-
-
 function mkStubRoomWithInviteRule(roomId: string, name: string, client: MatrixClient, joinRule: JoinRule): Room {
-    const stubRoom:Room = mkStubRoom(roomId,name,client);
-    stubRoom.getJoinRule =  jest.fn().mockReturnValue(joinRule);
+    const stubRoom: Room = mkStubRoom(roomId,name,client);
+    stubRoom.getJoinRule = jest.fn().mockReturnValue(joinRule);
     stubRoom.currentState.getJoinRule = jest.fn().mockReturnValue(joinRule);
     return stubRoom;
 }
@@ -22,9 +21,14 @@ const makeAccessEvent = (rule: TchapRoomAccessRule = TchapRoomAccessRule.Restric
     },
 } as any);
 
-function mkStubRoomWithAccessRule(roomId: string, name: string, client: MatrixClient, joinRule: JoinRule, accessRule: TchapRoomAccessRule): Room {
-    const stubRoom:Room = mkStubRoom(roomId,name,client);
-    stubRoom.getJoinRule =  jest.fn().mockReturnValue(joinRule);
+function mkStubRoomWithAccessRule(
+    roomId: string,
+    name: string,
+    client: MatrixClient,
+    joinRule: JoinRule,
+    accessRule: TchapRoomAccessRule): Room {
+    const stubRoom: Room = mkStubRoom(roomId, name, client);
+    stubRoom.getJoinRule = jest.fn().mockReturnValue(joinRule);
     stubRoom.currentState.getJoinRule = jest.fn().mockReturnValue(joinRule);
     const events = [
         makeAccessEvent(accessRule),
@@ -34,27 +38,25 @@ function mkStubRoomWithAccessRule(roomId: string, name: string, client: MatrixCl
 }
 
 describe("TchapJoinRule", () => {
-
-
-    beforeEach(() => {  });
+    beforeEach(() => {});
 
     it("should render the tchap join rule with only private option", () => {
-        //build stub private room 
+        //build stub private room
         const props = {
             room: mkStubRoomWithInviteRule("roomId", "roomName", createTestClient(), JoinRule.Invite),
-            closeSettingsFn(){},
-            onError(error: Error){},
-        }
-        
+            closeSettingsFn() {},
+            onError(error: Error) {},
+        };
+
         //arrange
         render(<TchapJoinRuleSettings {...props} />);
 
         //assert that spaces option is not here while private and public are
-        const publicText = "Public"
-        const privateText = "Private (invite only)"
-        const allowExternalText = "Allow external users to join this room"
-        const spaceText = "Anyone in a space can find and join"
-        
+        const publicText = "Public";
+        const privateText = "Private (invite only)";
+        const allowExternalText = "Allow external users to join this room";
+        const spaceText = "Anyone in a space can find and join";
+
         expect(screen.queryByText(publicText)).toBe(null);
         expect(screen.queryByText(privateText)).toBeDefined();
         expect(screen.queryByText(allowExternalText)).toBe(null);
@@ -62,22 +64,23 @@ describe("TchapJoinRule", () => {
     });
 
     it("should render the tchap join rule with only private option with restricted access rules", () => {
-        //build stub private room 
+        //build stub private room
         const props = {
-            room: mkStubRoomWithAccessRule("roomId", "roomName", createTestClient(), JoinRule.Invite, TchapRoomAccessRule.Restricted),
-            closeSettingsFn(){},
-            onError(error: Error){},
-        }
-        
+            room: mkStubRoomWithAccessRule(
+                "roomId", "roomName", createTestClient(), JoinRule.Invite, TchapRoomAccessRule.Restricted),
+            closeSettingsFn() {},
+            onError(error: Error) {},
+        };
+
         //arrange
         render(<TchapJoinRuleSettings {...props} />);
 
         //assert that spaces option is not here while private and public are
-        const publicText = "Public"
-        const privateText = "Private (invite only)"
-        const allowExternalText = "Allow external users to join this room"
-        const spaceText = "Anyone in a space can find and join"
-        
+        const publicText = "Public";
+        const privateText = "Private (invite only)";
+        const allowExternalText = "Allow external users to join this room";
+        const spaceText = "Anyone in a space can find and join";
+
         expect(screen.queryByText(publicText)).toBe(null);
         expect(screen.queryByText(privateText)).toBeDefined();
         expect(screen.queryByText(allowExternalText)).toBeDefined();
@@ -87,13 +90,13 @@ describe("TchapJoinRule", () => {
     /*
     Impossible to mock a room to be considered as Public from TchapJoinRuleSettings point of view
     it("should render the tchap join rule with only public option", () => {
-        //build stub private room 
+        //build stub private room
         const props = {
             room: mkStubRoomWithInviteRule("roomId", "roomName", createTestClient(), JoinRule.Public),
             closeSettingsFn(){},
             onError(error: Error){},
         }
-        
+
         //arrange
         render(<TchapJoinRuleSettings {...props} />);
 
@@ -101,11 +104,10 @@ describe("TchapJoinRule", () => {
         const privateText = "Private (invite only)"
         const publicText = "Public"
         const spaceText = "Anyone in a space can find and join"
-        
+
         expect(screen.getByText(privateText)).toBe(null);
         expect(screen.getByText(publicText)).toBeDefined();
         expect(screen.queryByText(spaceText)).toBe(null);
     });
     */
-
 });
