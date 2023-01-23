12b077be5d4475d5086a33430e5fbec029e7bda0
Estelle Comment <estelle.comment@gmail.com>
2023-01-23 12:39:55 +0100

    Add check for missing entries in patch file

--- scripts/checkPatchFile.js
+++ scripts/checkPatchFile.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function checkForDuplicateFiles(patchFile) {
   console.log("Checking patch file for files that are patched twice...");
   const findFirstDuplicate = (arr) => {
@@ -23,11 +25,11 @@ function checkForDuplicateFiles(patchFile) {
   console.log("... no files that are patched twice.");
 };
 
-function checkForMissingPatchDirs(patchFile) {
+function checkForMissingPatchDirs(patchFile, patchDirs) {
   console.log("Checking for missing patch directories...");
-  const patchDirs = []; // todo list dirs
-  Object.keys(patchFile).forEach(patchName => {
-    const found = patchDirs.find(patchDir => patchDir = patchName);
+  const patchNames = Object.keys(patchFile);
+  patchNames.forEach(patchName => {
+    const found = patchDirs.find(patchDir => patchDir === patchName);
     if (!found) {
       console.error("\nPATCH PROBLEM!")
       console.error("The patch", patchName, "is listed in patches.json but there is no corresponding directory in /patches.");
@@ -39,6 +41,30 @@ function checkForMissingPatchDirs(patchFile) {
   console.log("... no missing patch directories.");
 }
 
+function checkForMissingElementsInPatchFile(patchFile, patchDirs) {
+  console.log("Checking for missing elements in patches.json...");
+  const patchNames = Object.keys(patchFile);
+  patchDirs.forEach(patchDir => {
+    const found = patchNames.find(patchName => patchName === patchDir);
+    if (!found) {
+      console.error("\nPATCH PROBLEM!")
+      console.error("The patch", patchDir, "has a directory in /patches, but it is not listed in patches.json.");
+      console.error("Either add it to patches.json, or remove the directory in /patches.");
+      console.error("\n");
+      process.exit(1);
+    }
+  });
+  console.log("... no missing elements in patches.json.");
+}
+
+const getPatchDirs = () => {
+  return fs.readdirSync("patches", { withFileTypes: true })
+    .filter(file => file.isDirectory())
+    .map(dir => dir.name);
+}
+
 const patchFile = require('../patches/patches.json');
+const patchDirs = getPatchDirs();
 checkForDuplicateFiles(patchFile);
-checkForMissingPatchDirs(patchFile);
+checkForMissingPatchDirs(patchFile, patchDirs);
+checkForMissingElementsInPatchFile(patchFile, patchDirs);
