bbd471ed2e9f4f5e100d3ad802e895bd97ac7c35
Estelle Comment <estelle.comment@gmail.com>
2023-01-23 12:18:43 +0100

    Check for missing patch dirs

--- scripts/checkPatchFile.js
+++ scripts/checkPatchFile.js
@@ -1,10 +1,10 @@
-function checkPatchFile(patchFile) {
-  console.log("Checking patch file is valid...");
+function checkForDuplicateFiles(patchFile) {
+  console.log("Checking patch file for files that are patched twice...");
   const findFirstDuplicate = (arr) => {
-    let sorted_arr = arr.slice().sort();
-    for (let i = 0; i < sorted_arr.length - 1; i++) {
-      if (sorted_arr[i + 1] == sorted_arr[i]) {
-        return sorted_arr[i];
+    const sortedArr = arr.slice().sort();
+    for (let i = 0; i < sortedArr.length - 1; i++) {
+      if (sortedArr[i + 1] == sortedArr[i]) {
+        return sortedArr[i];
       }
     }
     return;
@@ -20,8 +20,25 @@ function checkPatchFile(patchFile) {
     console.error("The changes of the two packages will be mixed up. Don't do this.");
     process.exit(1);
   }
-  console.log("... patch file is valid.");
+  console.log("... no files that are patched twice.");
 };
 
+function checkForMissingPatchDirs(patchFile) {
+  console.log("Checking for missing patch directories...");
+  const patchDirs = []; // todo list dirs
+  Object.keys(patchFile).forEach(patchName => {
+    const found = patchDirs.find(patchDir => patchDir = patchName);
+    if (!found) {
+      console.error("\nPATCH PROBLEM!")
+      console.error("The patch", patchName, "is listed in patches.json but there is no corresponding directory in /patches.");
+      console.error("Either remove it from patches.json, or use yarn patch-make to make the missing patch file.");
+      console.error("\n");
+      process.exit(1);
+    }
+  });
+  console.log("... no missing patch directories.");
+}
+
 const patchFile = require('../patches/patches.json');
-checkPatchFile(patchFile);
+checkForDuplicateFiles(patchFile);
+checkForMissingPatchDirs(patchFile);
