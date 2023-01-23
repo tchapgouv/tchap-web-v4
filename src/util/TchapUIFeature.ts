a2d0a6b59d0a91066d729a8c1ee3976e73972fbf
Estelle Comment <estelle.comment@gmail.com>
2023-01-23 11:27:12 +0100

    Fix linter : add accessibility modifiers

--- src/util/TchapUIFeature.ts
+++ src/util/TchapUIFeature.ts
@@ -7,20 +7,20 @@ export default class TchapUIFeature {
      * This flag controls weither space related settings should be displayed or not. It differs from the flag UIComponent.CreateSpaces.
      * It is intended to add more fine-grained control over spaces disablement.
      */
-    static isSpaceDisplayEnabled = false;
+    public static isSpaceDisplayEnabled = false;
 
     /**
      * This flag controls weither Terms and Conditions should be accepted automatically or not.
      */
-    static autoAcceptTermsAndConditions = true;
+    public static autoAcceptTermsAndConditions = true;
 
     /**
      * This flag controls weither Email, Phone and Discovery UI should be displayed or not in General Settings.
      */
-    static showEmailPhoneDiscoverySettings = false;
+    public static showEmailPhoneDiscoverySettings = false;
 
     /**
      * This flag controls weither clearCacheAndReload can be queued at application start at V4 upgrade
      */
-    static activateClearCacheAndReloadAtVersion4 = true;
+    public static activateClearCacheAndReloadAtVersion4 = true;
 }
