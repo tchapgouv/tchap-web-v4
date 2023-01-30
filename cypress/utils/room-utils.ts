5b2c66fe6f67f83682b0c4aa8f3ef074a01f8836
Estelle Comment <estelle.comment@gmail.com>
2023-01-30 14:38:27 +0100

    Add a return statement

--- cypress/utils/room-utils.ts
+++ cypress/utils/room-utils.ts
@@ -11,12 +11,12 @@ export default class RoomUtils {
     public static createPrivateWithExternalRoom(roomName: string): Chainable<string> {
         return cy.createRoom(TchapCreateRoom.roomCreateOptions(roomName, TchapRoomType.External, false).createOpts);
     }
-    public static openRoomAccessSettings(roomName: string): void {
+    public static openRoomAccessSettings(roomName: string): Chainable<JQuery<HTMLElement>> {
         //open room
         cy.get('[aria-label="'+roomName+'"]').click();
         cy.get('.mx_RoomHeader_chevron').click();
         cy.get('[aria-label="Paramètres"] > .mx_IconizedContextMenu_label').click();
-        cy.get('[data-testid="settings-tab-ROOM_SECURITY_TAB"] > .mx_TabbedView_tabLabel_text').click();
+        return cy.get('[data-testid="settings-tab-ROOM_SECURITY_TAB"] > .mx_TabbedView_tabLabel_text').click();
     }
 }
 
