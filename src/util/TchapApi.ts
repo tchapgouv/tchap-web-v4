2e3a85571509173844acc7fd5b3798f592dda194
olivier <odelcroi@gmail.com>
2023-02-17 09:33:20 +0100

    chore: comment unused routes

--- src/util/TchapApi.ts
+++ src/util/TchapApi.ts
@@ -2,17 +2,17 @@
  * File copied from v2.
  */
 export default {
-    hostBase: "https://matrix.",
-    infoFromEmailUrl: "/_matrix/identity/api/v1/info?medium=email&address=",
-    publicKeyUrl: "/_matrix/media_proxy/unstable/public_key",
-    scanEncryptedUrl: "/_matrix/media_proxy/unstable/scan_encrypted",
-    scanUnencryptedUrl: "/_matrix/media_proxy/unstable/scan/",
-    downloadUnencryptedUrl: "/_matrix/media_proxy/unstable/download/",
-    downloadEncryptedUrl: "/_matrix/media_proxy/unstable/download_encrypted",
-    downloadUnencryptedThumbnailUrl: "/_matrix/media_proxy/unstable/thumbnail/",
-    thumbnailParams: "?width=800&height=600&method=scale",
-    lookupUrl: "/_matrix/client/unstable/account/3pid/lookup",
+    // hostBase: "https://matrix.", #not used
+    // infoFromEmailUrl: "/_matrix/identity/api/v1/info?medium=email&address=", #not used
+    // publicKeyUrl: "/_matrix/media_proxy/unstable/public_key", #not used
+    // scanEncryptedUrl: "/_matrix/media_proxy/unstable/scan_encrypted", #not used
+    // scanUnencryptedUrl: "/_matrix/media_proxy/unstable/scan/", #not used
+    // downloadUnencryptedUrl: "/_matrix/media_proxy/unstable/download/", #not used
+    // downloadEncryptedUrl: "/_matrix/media_proxy/unstable/download_encrypted", #not used
+    // downloadUnencryptedThumbnailUrl: "/_matrix/media_proxy/unstable/thumbnail/", #not used
+    //thumbnailParams: "?width=800&height=600&method=scale", not used
+    //lookupUrl: "/_matrix/client/unstable/account/3pid/lookup", this endpoint is deleted from the mainlining
     accountValidityResendEmailUrl: "/_matrix/client/unstable/account_validity/send_mail",
     passwordRulesUrl: "/_matrix/client/r0/password_policy",
-    expiredInfoUrl: "/_matrix/client/r0/user/",
+    //expiredInfoUrl: "/_matrix/client/r0/user/", not used
 };
