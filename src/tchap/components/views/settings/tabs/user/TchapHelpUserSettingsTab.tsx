73710484f0b421b36582135f1f21d2fbf38f8f26
olivier <odelcroi@gmail.com>
2023-06-26 15:35:50 +0200

    replace patch by a component customisation

---
+++ src/tchap/components/views/settings/tabs/user/TchapHelpUserSettingsTab.tsx
@@ -0,0 +1,396 @@
+/*
+Copyright 2019 - 2023 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, { ReactNode } from "react";
+import { logger } from "matrix-js-sdk/src/logger";
+
+import AccessibleButton from "matrix-react-sdk/src/components/views/elements/AccessibleButton";
+import { _t } from "matrix-react-sdk/src/languageHandler";
+
+import { MatrixClientPeg } from "matrix-react-sdk/src/MatrixClientPeg";
+import SdkConfig from "matrix-react-sdk/src/SdkConfig";
+import Modal from "matrix-react-sdk/src/Modal";
+
+import PlatformPeg from "matrix-react-sdk/src/PlatformPeg";
+import UpdateCheckButton from "matrix-react-sdk/src/components/views/settings/UpdateCheckButton";
+import BugReportDialog from "matrix-react-sdk/src/components/views/dialogs/BugReportDialog";
+import CopyableText from "matrix-react-sdk/src/components/views/elements/CopyableText";
+import SettingsTab from "matrix-react-sdk/src/components/views/settings/tabs/SettingsTab";
+import { SettingsSection } from "matrix-react-sdk/src/components/views/settings/shared/SettingsSection";
+import SettingsSubsection, { SettingsSubsectionText } from "matrix-react-sdk/src/components/views/settings/shared/SettingsSubsection";
+import ExternalLink from "matrix-react-sdk/src/components/views/elements/ExternalLink";
+import MatrixClientContext from "matrix-react-sdk/src/contexts/MatrixClientContext";
+
+interface IProps {
+    closeSettingsFn: () => void;
+}
+
+interface IState {
+    appVersion: string | null;
+    canUpdate: boolean;
+}
+
+export default class HelpUserSettingsTab extends React.Component<IProps, IState> {
+    public static contextType = MatrixClientContext;
+    public context!: React.ContextType<typeof MatrixClientContext>;
+
+    public constructor(props: IProps) {
+        super(props);
+
+        this.state = {
+            appVersion: null,
+            canUpdate: false,
+        };
+    }
+
+    public componentDidMount(): void {
+        PlatformPeg.get()
+            ?.getAppVersion()
+            .then((ver) => this.setState({ appVersion: ver }))
+            .catch((e) => {
+                logger.error("Error getting vector version: ", e);
+            });
+        PlatformPeg.get()
+            ?.canSelfUpdate()
+            .then((v) => this.setState({ canUpdate: v }))
+            .catch((e) => {
+                logger.error("Error getting self updatability: ", e);
+            });
+    }
+
+    private getVersionInfo(): { appVersion: string; olmVersion: string } {
+        const brand = SdkConfig.get().brand;
+        const appVersion = this.state.appVersion || "unknown";
+        const olmVersionTuple = MatrixClientPeg.get().olmVersion;
+        const olmVersion = olmVersionTuple
+            ? `${olmVersionTuple[0]}.${olmVersionTuple[1]}.${olmVersionTuple[2]}`
+            : "<not-enabled>";
+
+        return {
+            appVersion: `${_t("%(brand)s version:", { brand })} ${appVersion}`,
+            olmVersion: `${_t("Olm version:")} ${olmVersion}`,
+        };
+    }
+
+    private onClearCacheAndReload = (): void => {
+        if (!PlatformPeg.get()) return;
+
+        // Dev note: please keep this log line, it's useful when troubleshooting a MatrixClient suddenly
+        // stopping in the middle of the logs.
+        logger.log("Clear cache & reload clicked");
+        MatrixClientPeg.get().stopClient();
+        MatrixClientPeg.get()
+            .store.deleteAllData()
+            .then(() => {
+                PlatformPeg.get()?.reload();
+            });
+    };
+
+    private onBugReport = (): void => {
+        Modal.createDialog(BugReportDialog, {});
+    };
+
+    private renderLegal(): ReactNode {
+        const tocLinks = SdkConfig.get().terms_and_conditions_links;
+        if (!tocLinks) return null;
+
+        const legalLinks: JSX.Element[] = [];
+        for (const tocEntry of tocLinks) {
+            legalLinks.push(
+                <div key={tocEntry.url}>
+                    <ExternalLink href={tocEntry.url}>{tocEntry.text}</ExternalLink>
+                </div>,
+            );
+        }
+
+        return (
+            <SettingsSubsection heading={_t("Legal")}>
+                <SettingsSubsectionText>{legalLinks}</SettingsSubsectionText>
+            </SettingsSubsection>
+        );
+    }
+
+    private renderCredits(): JSX.Element {
+        // Note: This is not translated because it is legal text.
+        // Also, &nbsp; is ugly but necessary.
+        return (
+            <SettingsSubsection heading={_t("Credits")}>
+                <SettingsSubsectionText>
+                    <ul>
+                        <li>
+                            {_t(
+                                "The <colr>twemoji-colr</colr> font is © <author>Mozilla Foundation</author> " +
+                                    "used under the terms of <terms>Apache 2.0</terms>.",
+                                {},
+                                {
+                                    colr: (sub) => (
+                                        <ExternalLink
+                                            href="https://github.com/matrix-org/twemoji-colr"
+                                            rel="noreferrer noopener"
+                                            target="_blank"
+                                        >
+                                            {sub}
+                                        </ExternalLink>
+                                    ),
+                                    author: (sub) => <ExternalLink href="https://mozilla.org">{sub}</ExternalLink>,
+                                    terms: (sub) => (
+                                        <ExternalLink
+                                            href="https://www.apache.org/licenses/LICENSE-2.0"
+                                            rel="noreferrer noopener"
+                                            target="_blank"
+                                        >
+                                            {sub}
+                                        </ExternalLink>
+                                    ),
+                                },
+                            )}
+                        </li>
+                        <li>
+                            {_t(
+                                "The <twemoji>Twemoji</twemoji> emoji art is © " +
+                                    "<author>Twitter, Inc and other contributors</author> used under the terms of " +
+                                    "<terms>CC-BY 4.0</terms>.",
+                                {},
+                                {
+                                    twemoji: (sub) => (
+                                        <ExternalLink href="https://twemoji.twitter.com/">{sub}</ExternalLink>
+                                    ),
+                                    author: (sub) => (
+                                        <ExternalLink href="https://twemoji.twitter.com/">{sub}</ExternalLink>
+                                    ),
+                                    terms: (sub) => (
+                                        <ExternalLink
+                                            href="https://creativecommons.org/licenses/by/4.0/"
+                                            rel="noreferrer noopener"
+                                            target="_blank"
+                                        >
+                                            {sub}
+                                        </ExternalLink>
+                                    ),
+                                },
+                            )}
+                        </li>
+                    </ul>
+                </SettingsSubsectionText>
+            </SettingsSubsection>
+        );
+    }
+
+    private getVersionTextToCopy = (): string => {
+        const { appVersion, olmVersion } = this.getVersionInfo();
+        return `${appVersion}\n${olmVersion}`;
+    };
+
+    public render(): React.ReactNode {
+
+        let updateButton: JSX.Element | undefined;
+        if (this.state.canUpdate) {
+            updateButton = <UpdateCheckButton />;
+        }
+
+        let bugReportingSection;
+        if (SdkConfig.get().bug_report_endpoint_url) {
+            bugReportingSection = (
+                <SettingsSubsection
+                    heading={_t("Bug reporting")}
+                    description={
+                        <>
+                            <SettingsSubsectionText>
+                                {_t(
+                                     "If you've submitted a bug to the Tchap team (via support or GitHub), debug logs can help " +
+                                     "us track down the problem.",
+                                )}
+                                
+                            </SettingsSubsectionText>
+                        </>
+                    }
+                >
+                    <AccessibleButton onClick={this.onBugReport} kind="primary">
+                        {_t("Submit debug logs")}
+                    </AccessibleButton>
+                    <SettingsSubsectionText>
+                        {_t(
+                            "To report a Matrix-related security issue, please read the Matrix.org " +
+                                "<a>Security Disclosure Policy</a>.",
+                            {},
+                            {
+                                a: (sub) => (
+                                    <ExternalLink href="https://matrix.org/security-disclosure-policy/">
+                                        {sub}
+                                    </ExternalLink>
+                                ),
+                            },
+                        )}
+                    </SettingsSubsectionText>
+                </SettingsSubsection>
+            );
+        }
+
+        const { appVersion, olmVersion } = this.getVersionInfo();
+
+        // :TCHAP: add Contact us section, proper FAQ section, Known Issues section
+        const supportEmail = "support@tchap.beta.gouv.fr";
+        const contactSection = (
+            <div className='mx_SettingsTab_section'>
+                <span className='mx_SettingsTab_subheading'>{ _t("Contact us") }</span>
+                <div className='mx_SettingsTab_subsectionText'>
+                    {
+                        _t(
+                            "If you have any difficulties with using Tchap, please contact us by email at <b>%(supportEmail)s</b>",
+                            { supportEmail: supportEmail },
+                            {
+                                "b": (sub) => <b>{ sub }</b>,
+                            }
+                        )
+                    }
+                </div>
+                <AccessibleButton
+                    kind="primary"
+                    element="a"
+                    href={"mailto:" + supportEmail}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    { _t("Contact us") }
+                    <i className='mx_ExternalLink_icon' />
+                </AccessibleButton>
+            </div>
+        );
+
+        const faqUrl = "https://www.tchap.gouv.fr/faq";
+        const faqSection = (
+            <div className='mx_SettingsTab_section'>
+                <span className='mx_SettingsTab_subheading'>{ _t("Frequently Asked Questions (FAQ)") }</span>
+                <div className='mx_SettingsTab_subsectionText'>
+                    {
+                        _t(
+                            "We have compiled a list of the questions our users ask the most frequently. Your question may be answered there."
+                        )
+                    }
+                </div>
+                <AccessibleButton
+                    kind="primary"
+                    element="a"
+                    href={ faqUrl }
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    { _t("Read the FAQ") }
+                    <i className='mx_ExternalLink_icon' />
+                </AccessibleButton>
+            </div>
+        );
+
+        const knownIssuesUrl = "https://github.com/tchapgouv/tchap-web-v4/issues";
+        const knownIssuesSection = (
+            <div className='mx_SettingsTab_section'>
+                <span className='mx_SettingsTab_subheading'>{ _t("Known issues") }</span>
+                <div className='mx_SettingsTab_subsectionText'>
+                    {
+                        _t(
+                            "This version of Tchap Web is currently in development. There are known issues that will be solved soon."
+                        )
+                    }
+                </div>
+                <AccessibleButton
+                    kind="primary"
+                    element="a"
+                    href={ knownIssuesUrl }
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    { _t("Read the Known Issues") }
+                    <i className='mx_ExternalLink_icon' />
+                </AccessibleButton>
+            </div>
+        );
+        // end :TCHAP:
+
+        return (
+            <SettingsTab>
+                <SettingsSection heading={_t("Help & About")}>
+                    { /* :TCHAP: added */ contactSection }
+                    { /* :TCHAP: added */ faqSection }
+                    { /* :TCHAP: moved down the page - bugReportingSection */}
+                    { /* :TCHAP: 
+                    <SettingsSubsection heading={_t("FAQ")} description={faqText}>
+                        <AccessibleButton kind="primary" onClick={this.onKeyboardShortcutsClicked}>
+                            {_t("Keyboard Shortcuts")}
+                        </AccessibleButton>
+                    </SettingsSubsection> */}
+                    { /* :TCHAP: added */ knownIssuesSection }
+                    { /* :TCHAP: moved from higher up the page */ bugReportingSection }
+                    <SettingsSubsection heading={_t("Versions")}>
+                        <SettingsSubsectionText>
+                            <CopyableText getTextToCopy={this.getVersionTextToCopy}>
+                                {appVersion}
+                                <br />
+                                {olmVersion}
+                                <br />
+                            </CopyableText>
+                            {updateButton}
+                        </SettingsSubsectionText>
+                    </SettingsSubsection>
+                    {this.renderLegal()}
+                    {this.renderCredits()}
+                    <SettingsSubsection heading={_t("Advanced")}>
+                        <SettingsSubsectionText>
+                            {_t(
+                                "Homeserver is <code>%(homeserverUrl)s</code>",
+                                {
+                                    homeserverUrl: MatrixClientPeg.get().getHomeserverUrl(),
+                                },
+                                {
+                                    code: (sub) => <code>{sub}</code>,
+                                },
+                            )}
+                        </SettingsSubsectionText>
+                        {MatrixClientPeg.get().getIdentityServerUrl() && (
+                            <SettingsSubsectionText>
+                                {_t(
+                                    "Identity server is <code>%(identityServerUrl)s</code>",
+                                    {
+                                        identityServerUrl: MatrixClientPeg.get().getIdentityServerUrl(),
+                                    },
+                                    {
+                                        code: (sub) => <code>{sub}</code>,
+                                    },
+                                )}
+                            </SettingsSubsectionText>
+                        )}
+                        <SettingsSubsectionText>
+                            <details>
+                                <summary>{_t("Access Token")}</summary>
+                                <b>
+                                    {_t(
+                                        "Your access token gives full access to your account." +
+                                            " Do not share it with anyone.",
+                                    )}
+                                </b>
+                                <CopyableText getTextToCopy={() => MatrixClientPeg.get().getAccessToken()}>
+                                    {MatrixClientPeg.get().getAccessToken()}
+                                </CopyableText>
+                            </details>
+                        </SettingsSubsectionText>
+                        <AccessibleButton onClick={this.onClearCacheAndReload} kind="danger">
+                            {_t("Clear cache and reload")}
+                        </AccessibleButton>
+                    </SettingsSubsection>
+                </SettingsSection>
+            </SettingsTab>
+        );
+    }
+}
