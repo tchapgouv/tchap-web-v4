394b9e6a66ddc8a73a498dff49497420f293fdb1
olivier <odelcroi@gmail.com>
2023-08-23 15:55:26 +0200

    remove depreacted code

--- src/tchap/util/TchapUserSettings.ts
+++ src/tchap/util/TchapUserSettings.ts
@@ -1,7 +1,3 @@
-import { SettingLevel } from "matrix-react-sdk/src/settings/SettingLevel";
-import SettingsStore from "matrix-react-sdk/src/settings/SettingsStore";
-
-import TchapUIFeature from "./TchapUIFeature";
 
 export default class TchapUserSettings {
     /**
@@ -10,12 +6,6 @@ export default class TchapUserSettings {
      */
     public static override() {
 
-        /* @deprecated 
-        if (!TchapUIFeature.isCrossSigningAndSecureStorageActive()) {
-            //hard code this option in to order to hide secure storage in the user info > sessions panel
-            console.log(":tchap: hardcode e2ee.manuallyVerifyAllSessions to true");
-            SettingsStore.setValue("e2ee.manuallyVerifyAllSessions", null, SettingLevel.DEVICE, true);
-        }
-        */
+        //override user settings here if needed
     }
 }
