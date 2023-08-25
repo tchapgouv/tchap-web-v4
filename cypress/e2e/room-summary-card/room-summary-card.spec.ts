751134dedb4661f14e7e5050f8b96efadb17c049
Estelle Comment <estelle.comment@gmail.com>
2023-08-25 11:11:04 +0200

    Cypress : use normal room name

--- cypress/e2e/room-summary-card/room-summary-card.spec.ts
+++ cypress/e2e/room-summary-card/room-summary-card.spec.ts
@@ -2,6 +2,7 @@
 
 import RoomUtils from "../../utils/room-utils";
 import RandomUtils from "../../utils/random-utils";
+import { normalize } from "../../../yarn-linked-dependencies/matrix-js-sdk/src/utils";
 
 describe("Export room members feature", () => {
     const homeserverUrl = Cypress.env("E2E_TEST_USER_HOMESERVER_URL");
@@ -9,8 +10,7 @@ describe("Export room members feature", () => {
     const email = Cypress.env("E2E_TEST_USER_EMAIL");
     const password = Cypress.env("E2E_TEST_USER_PASSWORD");
     const today = new Date().toISOString().slice(0, 10).replace(/-/g, "");
-    // todo normalize name
-    const roomName = "test" + /*today + "exportroommembers" + */ RandomUtils.generateRandom(4);
+    const roomName = "test/" + today + "/export_room_members/" + RandomUtils.generateRandom(4);
     // This will fail if email has special characters.
     const userId = "@" + email.replace('@', '-') + ":" + homeserverShort;
 
@@ -33,12 +33,13 @@ describe("Export room members feature", () => {
     // });
 
     it("downloads the file when button is clicked", () => {
+        const normalizedRoomName = normalize(roomName);
         RoomUtils.createPublicRoom(roomName).then((roomId) => {
             RoomUtils.openPeopleMenu(roomName);
             cy.get('[data-testid="exportRoomMembersButton"]')
                 .click()
                 .then(() => {
-                    cy.readFile("cypress/downloads/membres_de_" + roomName + ".txt")
+                    cy.readFile("cypress/downloads/membres_de_" + normalizedRoomName + ".txt")
                         .should("eq", userId);
                 });
         });
