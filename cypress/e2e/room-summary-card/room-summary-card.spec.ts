e2996dbc1ccb07c3adcd390f78e2906720707a5a
Estelle Comment <estelle.comment@gmail.com>
2023-08-25 11:31:02 +0200

    Cypress : leave room when done

--- cypress/e2e/room-summary-card/room-summary-card.spec.ts
+++ cypress/e2e/room-summary-card/room-summary-card.spec.ts
@@ -10,7 +10,6 @@ describe("Export room members feature", () => {
     const email = Cypress.env("E2E_TEST_USER_EMAIL");
     const password = Cypress.env("E2E_TEST_USER_PASSWORD");
     const today = new Date().toISOString().slice(0, 10).replace(/-/g, "");
-    const roomName = "test/" + today + "/export_room_members/" + RandomUtils.generateRandom(4);
     // This will fail if email has special characters.
     const userId = "@" + email.replace('@', '-') + ":" + homeserverShort;
 
@@ -19,21 +18,23 @@ describe("Export room members feature", () => {
     });
 
     afterEach(() => {
-        // todo delete room, otherwise the test user will end up with a million identical rooms after a while.
     });
 
     it('should display the tooltip on button hover', () => {
-        RoomUtils.createPublicRoom(roomName).then(() => {
+        const roomName = "test/" + today + "/export_room_members/" + RandomUtils.generateRandom(4);
+        RoomUtils.createPublicRoom(roomName).then((roomId) => {
             RoomUtils.openPeopleMenu(roomName);
             cy.get('[data-testid="tc_exportRoomMembersButton"]')
                 .trigger('mouseover')
                 .get('.tc_exportRoomMembersTooltip') // tooltip should show
+            cy.leaveRoom(roomId);
         });
      });
 
     it("downloads the file when button is clicked", () => {
+        const roomName = "test/" + today + "/export_room_members/" + RandomUtils.generateRandom(4);
         const normalizedRoomName = normalize(roomName);
-        RoomUtils.createPublicRoom(roomName).then(() => {
+        RoomUtils.createPublicRoom(roomName).then((roomId) => {
             RoomUtils.openPeopleMenu(roomName);
             cy.get('[data-testid="tc_exportRoomMembersButton"]')
                 .click()
@@ -41,6 +42,7 @@ describe("Export room members feature", () => {
                     cy.readFile("cypress/downloads/membres_de_" + normalizedRoomName + ".txt")
                         .should("eq", userId);
                 });
+            cy.leaveRoom(roomId);
         });
     });
 });
