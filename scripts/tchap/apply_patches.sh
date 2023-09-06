b1d4052ada2fb481555b7f7310f465a44224b906
Estelle Comment <estelle.comment@gmail.com>
2023-09-06 12:03:11 +0200

    Make patch-package output better errors

--- scripts/tchap/apply_patches.sh
+++ scripts/tchap/apply_patches.sh
@@ -3,6 +3,10 @@
 # Script to apply the patches in patches directory, using patch-package.
 set -e
 
+echo "Patching patch-package itself... (is https://github.com/ds300/patch-package/issues/488 fixed now?)"
+yarn patch-package --patch-dir patches_patch-package --error-on-fail
+echo "... done patching patch-package."
+
 node scripts/tchap/checkPatchFile.js
 
 for d in patches/*/ ; do
