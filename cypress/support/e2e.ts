52335e37e638a864ac4028641d073f0c1e9c91c6
Estelle Comment <estelle.comment@gmail.com>
2023-09-28 18:09:42 +0200

    Cleanup

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,7 +4,9 @@
 
 /// <reference types="cypress" />
 
+// Import matrix-react-sdk's support files
 import "../../node_modules/matrix-react-sdk/cypress/support/e2e.ts";
 
+// Add tchap's additions.
 import "./loginToRemoteHomeserver.ts";
 import "./leaveRoom.ts";
