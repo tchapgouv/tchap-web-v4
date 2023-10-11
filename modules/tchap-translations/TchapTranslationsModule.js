806e18576de9f72ce5af356bb1d7d48927d997ed
Estelle Comment <estelle.comment@gmail.com>
2023-10-11 15:38:20 +0200

    Import translations from file

--- modules/tchap-translations/TchapTranslationsModule.js
+++ modules/tchap-translations/TchapTranslationsModule.js
@@ -3,15 +3,11 @@ Copyright 2023 DINUM
 */
 import { RuntimeModule } from "@matrix-org/react-sdk-module-api/lib/RuntimeModule";
 
+import tchapTranslations from "./tchap_translations.json";
 export default class TchapTranslationsModule extends RuntimeModule {
     constructor(moduleApi) {
         super(moduleApi);
 
-        this.moduleApi.registerTranslations({
-            "Explore public rooms": {
-                "en": "Explore public roommmmmms",
-                "fr": "Rejoindre un forumnnnnn"
-            },
-        });
+        this.moduleApi.registerTranslations(tchapTranslations);
     }
 }
