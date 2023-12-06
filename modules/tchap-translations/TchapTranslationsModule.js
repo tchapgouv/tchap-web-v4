b7b01ec7b9b76203c435258505deb4e2cb8cdd08
Estelle Comment <estelle.comment@gmail.com>
2023-12-06 14:51:47 +0100

    Use two separate translation files

--- modules/tchap-translations/TchapTranslationsModule.js
+++ modules/tchap-translations/TchapTranslationsModule.js
@@ -3,11 +3,16 @@ Copyright 2023 DINUM
 */
 import { RuntimeModule } from "@matrix-org/react-sdk-module-api/lib/RuntimeModule";
 
-import tchapTranslations from "./tchap_translations.json";
+import tchapTranslationsWeb from "./tchap_translations_web.json";
+import tchapTranslationsReactSdk from "./tchap_translations_react-sdk.json";
+
 export default class TchapTranslationsModule extends RuntimeModule {
     constructor(moduleApi) {
         super(moduleApi);
 
-        this.moduleApi.registerTranslations(tchapTranslations);
+        const allTranslations = { ...tchapTranslationsWeb, ...tchapTranslationsReactSdk };
+        // Note : in case of conflict, the last translation file will override the one before it.
+
+        this.moduleApi.registerTranslations(allTranslations);
     }
 }
