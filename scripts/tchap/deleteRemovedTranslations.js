c28e3b541c5900a71e78607ceec4d19b65a6e88a
Estelle Comment <estelle.comment@gmail.com>
2023-12-12 14:40:54 +0100

    Make script work for nested translations

--- scripts/tchap/deleteRemovedTranslations.js
+++ scripts/tchap/deleteRemovedTranslations.js
@@ -6,7 +6,29 @@ const tchapTranslations = require(argv.file);
 const toRemove = require(argv.toremove);
 
 for (const key of toRemove) {
-  delete tchapTranslations[key];
+  // We could have key = "auth|do_stuff", in which case we should deal with the nesting.
+  const keyPieces = key.split("|");
+  // Officially the ugliest piece of script of all tchap. Use some elegant recursive crap or ES6 voodoo.
+  switch (keyPieces.length) {
+    case 1:
+      delete tchapTranslations[keyPieces[0]];
+      break;
+    case 2:
+      delete tchapTranslations[keyPieces[0]][keyPieces[1]];
+      break;
+    case 3:
+      delete tchapTranslations[keyPieces[0]][keyPieces[1]][keyPieces[2]];
+      break;
+    case 4:
+      delete tchapTranslations[keyPieces[0]][keyPieces[1]][keyPieces[2]][keyPieces[3]];
+      break;
+    case 5:
+      delete tchapTranslations[keyPieces[0]][keyPieces[1]][keyPieces[2]][keyPieces[3]][keyPieces[4]];
+      break;
+    default:
+      // I'm counting on this never happening.
+      throw new Error("More than 5 layers of nesting of translations??? Adapt the script.");
+  }
 }
 
 console.log(JSON.stringify(tchapTranslations));
