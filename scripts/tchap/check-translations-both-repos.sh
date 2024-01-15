2ca0504664f8d1e8dc656f35ce46c7d5738e5364
Estelle Comment <estelle.comment@gmail.com>
2024-01-15 16:13:24 +0100

    Add in the terms and conditions links by hand

--- scripts/tchap/check-translations-both-repos.sh
+++ scripts/tchap/check-translations-both-repos.sh
@@ -21,6 +21,15 @@ yarn i18n:lint # lints the whole src/i18n/strings/ dir, no need to modify # todo
 # Merge the crawled files from both repos
 # Note : this works with nested keys.
 jq -s '.[0] * .[1]' $CRAWLED_WEB $CRAWLED_REACT > $CRAWLED_BOTH
+
+# Extra hack : config.json is not crawled by matrix-gen-i18n, the terms_and_conditions_links are missing. Add them in.
+# Get the terms_and_conditions strings from config.json
+export TOC=`realpath modules/tchap-translations/tmp/terms_and_conditions.json`
+jq '.terms_and_conditions_links[] | { (.text): .text} ' config.json | jq -s add > $TOC
+# Merge them into CRAWLED_BOTH
+# Note : this works with nested keys.
+jq -s '.[0] * .[1]' $TOC $CRAWLED_BOTH > $CRAWLED_BOTH.tmp && mv $CRAWLED_BOTH.tmp $CRAWLED_BOTH
+
 # Format the file for clean diffing.
 jq --sort-keys '.' $CRAWLED_BOTH > $CRAWLED_BOTH.tmp && mv $CRAWLED_BOTH.tmp $CRAWLED_BOTH # yarn i18n:sort with customized files
 yarn i18n:lint # lints the whole src/i18n/strings/ dir, no need to modify # todo is this useful ?
