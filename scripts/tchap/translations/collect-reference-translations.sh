68efa102f4f49658e75e30dcca9c141b5563dc59
Estelle Comment <estelle.comment@gmail.com>
2024-01-16 16:12:49 +0100

    Move helper functions out

--- scripts/tchap/translations/collect-reference-translations.sh
+++ scripts/tchap/translations/collect-reference-translations.sh
@@ -4,18 +4,7 @@
 # and merge them all together.
 # Usage : ./scripts/tchap/translations/collect-reference-translations.sh $REFERENCE_TRANSLATIONS
 
-# Usage : merge_json_files file1 file2 outputfile
-merge_json_files () {
-    # Note : this works with nested keys.
-    # Note : in case of conflict, the values in the file given in second argument will override.
-    jq -s '.[0] * .[1]' $1 $2 > $3.tmp && mv $3.tmp $3
-}
-
-# Usage : format_json_file file
-format_json_file () {
-    # Format the file for clean diffing.
-    jq --sort-keys '.' $1 > $1.tmp && mv $1.tmp $1 # yarn i18n:sort with customized files
-}
+source scripts/tchap/translations/helpers.sh
 
 # Usage : collect_reference_translations repo element_translation_file output_file
 collect_reference_translations () {
--- scripts/tchap/translations/crawl-for-translations.sh
+++ scripts/tchap/translations/crawl-for-translations.sh
@@ -4,18 +4,7 @@
 # to the output file.
 # Usage : ./scripts/tchap/translations/crawl-for-translations.sh $REFERENCE_TRANSLATIONS $OUTPUT
 
-# Usage : merge_json_files file1 file2 outputfile
-merge_json_files () {
-    # Note : this works with nested keys.
-    # Note : in case of conflict, the values in the file given in second argument will override.
-    jq -s '.[0] * .[1]' $1 $2 > $3.tmp && mv $3.tmp $3
-}
-
-# Usage : format_json_file file
-format_json_file () {
-    # Format the file for clean diffing.
-    jq --sort-keys '.' $1 > $1.tmp && mv $1.tmp $1 # yarn i18n:sort with customized files
-}
+source scripts/tchap/translations/helpers.sh
 
 # Usage : crawl repo inputfile outputfile
 crawl () {
