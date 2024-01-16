36eb1b8c33f3cecfd94b53474ca599afb395a245
Estelle Comment <estelle.comment@gmail.com>
2024-01-16 15:09:52 +0100

    Add mkdir to fix bug

--- scripts/tchap/translations/check-translations-both-repos.sh
+++ scripts/tchap/translations/check-translations-both-repos.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+mkdir -p `realpath modules/tchap-translations/tmp`
 
-./scripts/tchap/translations/collect-reference-translations.sh
 export REFERENCE_TRANSLATIONS=`realpath modules/tchap-translations/tmp/reference_both.json` # todo pass this as argument
+./scripts/tchap/translations/collect-reference-translations.sh
 
-./scripts/tchap/translations/crawl-for-translations.sh
 export CRAWLED=`realpath modules/tchap-translations/tmp/crawled_both.json` # todo pass this as argument
+./scripts/tchap/translations/crawl-for-translations.sh
 
 # Diff the two.
 echo "Comparing:"
