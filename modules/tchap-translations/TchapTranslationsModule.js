3ce89e9352d967c31e120704ffbbc1f488b16f1a
Estelle Comment <estelle.comment@gmail.com>
2024-01-22 13:58:55 +0100

    Revert TchapTranslationModule to single translation file

--- modules/tchap-translations/TchapTranslationsModule.js
+++ modules/tchap-translations/TchapTranslationsModule.js
@@ -3,16 +3,11 @@ Copyright 2023 DINUM
 */
 import { RuntimeModule } from "@matrix-org/react-sdk-module-api/lib/RuntimeModule";
 
-import tchapTranslationsWeb from "./tchap_translations_web.json";
-import tchapTranslationsReactSdk from "./tchap_translations_react-sdk.json";
-
+import tchapTranslations from "./tchap_translations.json";
 export default class TchapTranslationsModule extends RuntimeModule {
     constructor(moduleApi) {
         super(moduleApi);
 
-        const allTranslations = { ...tchapTranslationsWeb, ...tchapTranslationsReactSdk };
-        // Note : in case of conflict, the last translation file will override the one before it.
-
-        this.moduleApi.registerTranslations(allTranslations);
+        this.moduleApi.registerTranslations(tchapTranslations);
     }
 }
