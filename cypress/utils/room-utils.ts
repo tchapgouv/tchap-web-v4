c2f0a89c00ee953549d9ca8e86ba8eb09e16c9f1
Estelle Comment <estelle.comment@gmail.com>
2024-02-09 11:24:33 +0100

    Update cypress tests

--- cypress/utils/room-utils.ts
+++ cypress/utils/room-utils.ts
@@ -9,15 +9,15 @@ export default class RoomUtils {
     }
     public static openRoomAccessSettings(roomName: string): Chainable<JQuery<HTMLElement>> {
         this.openRoomInformation(roomName);
-        cy.get(".mx_RoomSummaryCard_Button.mx_RoomSummaryCard_icon_settings").click();
+        cy.get(".mx_RoomSummaryCard").contains("Paramètres").click();
         return cy.get('[data-testid="settings-tab-ROOM_SECURITY_TAB"] > .mx_TabbedView_tabLabel_text').click();
     }
     public static openRoomInformation(roomName: string): Chainable<JQuery<HTMLElement>> {
         cy.get('[aria-label="' + roomName + '"]').click(); //open room
-        return cy.get('.mx_RoomHeader_info [title="' + roomName + '"]').click();
+        return cy.get(".mx_RoomHeader_infoWrapper").contains(roomName).click();
     }
     public static openPeopleMenu(roomName: string): Chainable<JQuery<HTMLElement>> {
         this.openRoomInformation(roomName);
-        return cy.get(".mx_RoomSummaryCard_icon_people").click();
+        return cy.get(".mx_RoomSummaryCard").contains("Personnes").click();
     }
 }
