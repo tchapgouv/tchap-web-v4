3f8722e8daa3b45ddc298a5154898d52f395f25c
Estelle Comment <estelle.comment@gmail.com>
2024-03-26 16:00:23 +0100

    More messy testing. We need to refactor the prod code.

--- test/unit-tests/tchap/rageshake/submit-rageshake-test.ts
+++ test/unit-tests/tchap/rageshake/submit-rageshake-test.ts
@@ -1,19 +1,54 @@
+//jest.mock("matrix-react-sdk/src/rageshake/submit-rageshake"); //, () => ({
+//    exportedSubmitReportForTesting: jest.fn(() => Promise.resolve("coucou")), // -> mock not called by prod code.
+//}));
+//const mockedAxios = axios as jest.Mocked<typeof "matrix-react-sdk/src/rageshake/submit-rageshake">;
+
 import { stubClient } from "matrix-react-sdk/test/test-utils";
-import sendBugReport, { submitReport } from "matrix-react-sdk/src/rageshake/submit-rageshake";
 import { MatrixClientPeg } from "matrix-react-sdk/src/MatrixClientPeg";
 import MockHttpBackend from "matrix-mock-request";
+
+import sendBugReport from "~tchap-web-dep/matrix-react-sdk/src/rageshake/submit-rageshake";
+import { exportedSubmitReportForTesting } from "~tchap-web/yarn-linked-dependencies/matrix-react-sdk/src/rageshake/submitReport";
 //import * as Rageshake from "matrix-react-sdk/src/rageshake/submit-rageshake"; // weird import for testing
 //import sendBugReport from "matrix-react-sdk/src/rageshake/submit-rageshake";
 
+jest.mock("~tchap-web-dep/matrix-react-sdk/src/rageshake/submitReport", () => ({
+    exportedSubmitReportForTesting: jest.fn(() => Promise.resolve("coucou")), // -> mock not called by prod code.
+}));
+
+/*jest.mock("~tchap-web-dep/matrix-react-sdk/src/rageshake/submit-rageshake", () => {
+    const originalModule = jest.requireActual("~tchap-web-dep/matrix-react-sdk/src/rageshake/submit-rageshake");
+
+    //Mock the default export and named export 'foo'
+    return {
+        __esModule: true,
+        ...originalModule,
+        //default: jest.fn(() => "mocked baz"),
+        //foo: 'mocked foo',
+        exportedSubmitReportForTesting: jest.fn(() => "mocked baz"),
+    };
+});*/
+
 describe("submit-rageshake", () => {
     let httpBackend: MockHttpBackend;
+    let mock;
 
     beforeEach(() => {
         httpBackend = new MockHttpBackend();
         stubClient();
+        //mock = jest.fn(() => Promise.resolve("coucou"));
+        //jest.mock(exportedSubmitReportForTesting).mockImplementation(mock);
+        //exportedSubmitReportForTesting.mockImplementation(mock);
+        //exportedSubmitReportForTesting = mock;
+        //spy = jest.spyOn(exportedSubmitReportForTesting);
     });
 
-    /*    it("should pass user email and app name to the http request", async () => {
+    // todo after : clean up mock ?
+
+    // try mocking submitReport -> the mock is not used by the prod code.
+    // because the prod code calls within the file, not through an import, I think. Is it ?
+    // If it is, can the prod code call submitReport differently ? Call exportedSubmitReportForTesting instead ?
+    it("should pass user email and app name to the http request", async () => {
         const client = MatrixClientPeg.safeGet();
         client.getThreePids = jest
             .fn()
@@ -23,22 +58,19 @@ describe("submit-rageshake", () => {
         client.getCrypto = () => undefined;
         client.http = { request: () => Promise.resolve({}) };
 
-        jest.mock("matrix-react-sdk/src/rageshake/submit-rageshake", () => ({
-            submitReport: jest.fn(() => Promise.resolve({ id: 1, name: "John Doe" })),
-        }));
-
         // Spy submitReport and don't call through (this is probably excessively hacky)
         // const mySpy = jest.spyOn(Rageshake, "exportedSubmitReportForTesting").mockImplementation(() => {});
 
         const opts = {};
-        await sendBugReport("/go/bugreport/go", opts);
+        const result = await sendBugReport("/go/bugreport/go", opts);
+        console.log("RESULT", result);
 
-        expect(submitReport).toHaveBeenCalledWith({});
-    });*/
+        expect(exportedSubmitReportForTesting).toHaveBeenCalledWith({});
+    });
 
     // attempt to mock out XMLHttpRequest, it's too painful
     // needs to trigger xhrMock.onreadystatechange after the call to sendBugReport, but before it resolves.
-    it("should pass user email and app name to the http request", (done) => {
+    /*    it("should pass user email and app name to the http request", (done) => {
         // spy on http request
         //jest.spyOn(XMLHttpRequest.prototype, "open").mockReturnValue();
         //jest.spyOn(XMLHttpRequest.prototype, "send").mockReturnValue();
@@ -83,9 +115,10 @@ describe("submit-rageshake", () => {
         //expect(xhrMock.open).toBeCalledWith('GET', 'https://example.com/');
         //expect(xhrMock.setRequestHeader).toBeCalledWith('Accept', 'application/json');
         //expect(callback.mock.calls).toEqual([["Hello World!"]]);
-    });
+    });*/
 
     // attempt with mock backend -> mock backend is not used.
+    // We could do it with no backend, real request to fake url, and handle the econn error properly. Right now something hangs.
     /*    it("should pass user email and app name to the http request", (done) => {
         // try this maybe ?
         //        httpBackend.when("POST", "/go/bugreport/go").check(function (req) {
