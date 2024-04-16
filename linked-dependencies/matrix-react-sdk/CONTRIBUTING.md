1cef912f8eddf64aef5bb2a63c372b0e594e6bb0
Estelle Comment <estelle.comment@gmail.com>
2024-04-16 10:02:15 +0200

    Remove the subtree so that I can diff the other files

--- linked-dependencies/matrix-react-sdk/CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Contributing code to matrix-react-sdk
-
-matrix-react-sdk follows the same pattern as https://github.com/vector-im/element-web/blob/develop/CONTRIBUTING.md
--- linked-dependencies/matrix-react-sdk/playwright/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-yarn link
-yarn --cwd ../element-web install
-yarn --cwd ../element-web link matrix-react-sdk
-npx playwright test --update-snapshots --reporter line --project='Legacy Crypto' $@
