cebf441643a9486477d04f70224b1b5839b17321
marc.sirisak <marc.sirisak@beta.gouv.fr>
2024-06-24 17:35:21 +0200

    fix(build): add subtree node_modules during postinstall

--- scripts/tchap/postinstall.sh
+++ scripts/tchap/postinstall.sh
@@ -7,9 +7,13 @@ set -x
 
 cd linked-dependencies
 pushd matrix-react-sdk
+# Normally when dealing with a subtree and node_modules, it is good practice to merge them in the parent package.json. 
+# However in our case nothing should overlaps since element and matrix-react-sdk are deeply interconnected.
+# Moreover, webpack will apply tree shaking to remove unecessary pacakges
+yarn install --pure-lockfile
 yarn unlink # :TCHAP: for local build, undo previous links if present.
 yarn link
-# yarn install --pure-lockfile
+
 popd
 
 cd ..
