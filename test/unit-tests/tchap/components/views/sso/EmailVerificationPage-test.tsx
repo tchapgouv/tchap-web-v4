19c277fcf5cef55de33e2cdcebc597d30d8e0227
olivier <odelcroi@gmail.com>
2025-07-31 15:03:40 +0200

    lint

--- test/unit-tests/tchap/components/views/sso/EmailVerificationPage-test.tsx
+++ test/unit-tests/tchap/components/views/sso/EmailVerificationPage-test.tsx
@@ -65,229 +65,226 @@ describe("Tests sso and oidc native flow", () => {
 
     // Créer un mock pour onServerConfigChange
     const onServerConfigChangeMock = jest.fn();
- 
-    const renderEmailVerificationPage = () => render(<EmailVerificationPage onServerConfigChange={onServerConfigChangeMock} />);
 
-describe("MAS flow deactivated", () => {
+    const renderEmailVerificationPage = () =>
+        render(<EmailVerificationPage onServerConfigChange={onServerConfigChangeMock} />);
 
+    describe("MAS flow deactivated", () => {
+        beforeEach(() => {
+            const config: ConfigOptions = { tchap_mas_flow: { isActive: false } };
+            SdkConfig.put(config);
 
-    beforeEach(() => {
-        const config: ConfigOptions = { tchap_mas_flow: { isActive: false } };
-        SdkConfig.put(config);
-
-        mockedLogin.mockImplementation(() => ({
-            hsUrl: defaultHsUrl,
-            createTemporaryClient: jest.fn().mockReturnValue(mockedClient),
-            getFlows: jest.fn().mockResolvedValue([{ type: "m.login.sso" }]),
-        }));
-    });
-
-    afterEach(() => {
-        cleanup();
-        jest.restoreAllMocks();
-    });
-
-    it("returns error when empty email", async () => {
-        renderEmailVerificationPage();
-
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: "" } });
-
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
+            mockedLogin.mockImplementation(() => ({
+                hsUrl: defaultHsUrl,
+                createTemporaryClient: jest.fn().mockReturnValue(mockedClient),
+                getFlows: jest.fn().mockResolvedValue([{ type: "m.login.sso" }]),
+            }));
         });
 
-        // Submit button should be disabled
-        expect(proconnectButton).toHaveAttribute("disabled");
+        afterEach(() => {
+            cleanup();
+            jest.restoreAllMocks();
+        });
+
+        it("returns error when empty email", async () => {
+            renderEmailVerificationPage();
+
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: "" } });
+
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            // Submit button should be disabled
+            expect(proconnectButton).toHaveAttribute("disabled");
+        });
+
+        it("returns inccorrect email", async () => {
+            renderEmailVerificationPage();
+
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: "falseemail" } });
+
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            // Submit button should be disabled
+            expect(proconnectButton).toHaveAttribute("disabled");
+        });
+
+        it("should throw error when homeserver catch an error", async () => {
+            const { container } = renderEmailVerificationPage();
+
+            // mock server returns an errorn, we dont need to mock the other implementation
+            // since the code should throw an error before accessing them
+            mockedValidatedServerConfig(true);
+
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: userEmail } });
+
+            await flushPromises();
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            // Error classes should not appear
+            expect(container.getElementsByClassName("mx_ErrorMessage").length).toBe(1);
+        });
+
+        it("should throw and error when connecting to proconnect error", async () => {
+            const { container } = renderEmailVerificationPage();
+
+            mockedValidatedServerConfig(false);
+            // mock platform page startsso error
+            mockedPlatformPegStartSSO(true);
+
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: userEmail } });
+
+            await flushPromises();
+
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            // Error classes should not appear
+            expect(container.getElementsByClassName("mx_ErrorMessage").length).toBe(1);
+        });
+
+        it("should start sso with correct homeserver 1", async () => {
+            renderEmailVerificationPage();
+
+            // Mock the implementation without error, what we want is to be sure they are called with the correct parameters
+            mockedFetchHomeserverFromEmail(defaultHsUrl);
+            mockedValidatedServerConfig(false, defaultHsUrl);
+            mockedPlatformPegStartSSO(false);
+
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: userEmail } });
+
+            await flushPromises();
+
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            expect(mockedTchapUtils.makeValidatedServerConfig).toHaveBeenCalledWith({
+                base_url: defaultHsUrl,
+                server_name: defaultHsUrl,
+            });
+
+            expect(PlatformPegMocked.startSingleSignOn).toHaveBeenNthCalledWith(
+                1,
+                mockedClient,
+                "sso",
+                "/home",
+                "",
+                SSOAction.LOGIN,
+                userEmail,
+            );
+        });
+
+        it("should start sso with correct homeserver 2", async () => {
+            renderEmailVerificationPage();
+
+            // Mock the implementation without error, what we want is to be sure they are called with the correct parameters
+            mockedFetchHomeserverFromEmail(secondHsUrl);
+            mockedValidatedServerConfig(false, secondHsUrl);
+            mockedPlatformPegStartSSO(false);
+
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: userEmail } });
+
+            await flushPromises();
+
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            expect(mockedTchapUtils.makeValidatedServerConfig).toHaveBeenCalledWith({
+                base_url: secondHsUrl,
+                server_name: secondHsUrl,
+            });
+
+            expect(PlatformPegMocked.startSingleSignOn).toHaveBeenNthCalledWith(
+                1,
+                mockedClient,
+                "sso",
+                "/home",
+                "",
+                SSOAction.LOGIN,
+                userEmail,
+            );
+        });
+
+        it("should display error when sso is not configured in homeserer", async () => {
+            const { container } = renderEmailVerificationPage();
+
+            // Mock the implementation without error, what we want is to be sure they are called with the correct parameters
+            mockedFetchHomeserverFromEmail(secondHsUrl);
+            mockedValidatedServerConfig(false, secondHsUrl);
+            mockedPlatformPegStartSSO(false);
+            // get flow without sso configured on homeserver
+            mockedLogin.mockImplementation(() => ({
+                hsUrl: secondHsUrl,
+                createTemporaryClient: jest.fn().mockReturnValue(mockedClient),
+                getFlows: jest.fn().mockResolvedValue([{ type: "m.login.password" }]),
+            }));
+            // Put text in email field
+            const emailField = screen.getByRole("textbox");
+            fireEvent.focus(emailField);
+            fireEvent.change(emailField, { target: { value: userEmail } });
+
+            await flushPromises();
+
+            // click on proconnect button
+            const proconnectButton = screen.getByTestId("proconnect-submit");
+            await act(async () => {
+                await fireEvent.click(proconnectButton);
+            });
+
+            expect(container.getElementsByClassName("mx_ErrorMessage").length).toBe(1);
+        });
     });
-
-    it("returns inccorrect email", async () => {
-        renderEmailVerificationPage();
-
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: "falseemail" } });
-
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
-        });
-
-        // Submit button should be disabled
-        expect(proconnectButton).toHaveAttribute("disabled");
-    });
-
-    it("should throw error when homeserver catch an error", async () => {
-        const { container } = renderEmailVerificationPage();
-
-        // mock server returns an errorn, we dont need to mock the other implementation
-        // since the code should throw an error before accessing them
-        mockedValidatedServerConfig(true);
-
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: userEmail } });
-
-        await flushPromises();
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
-        });
-
-        // Error classes should not appear
-        expect(container.getElementsByClassName("mx_ErrorMessage").length).toBe(1);
-    });
-
-    it("should throw and error when connecting to proconnect error", async () => {
-        const { container } = renderEmailVerificationPage();
-
-        mockedValidatedServerConfig(false);
-        // mock platform page startsso error
-        mockedPlatformPegStartSSO(true);
-
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: userEmail } });
-
-        await flushPromises();
-
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
-        });
-
-        // Error classes should not appear
-        expect(container.getElementsByClassName("mx_ErrorMessage").length).toBe(1);
-    });
-
-    it("should start sso with correct homeserver 1", async () => {
-        renderEmailVerificationPage();
-
-        // Mock the implementation without error, what we want is to be sure they are called with the correct parameters
-        mockedFetchHomeserverFromEmail(defaultHsUrl);
-        mockedValidatedServerConfig(false, defaultHsUrl);
-        mockedPlatformPegStartSSO(false);
-
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: userEmail } });
-
-        await flushPromises();
-
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
-        });
-
-        expect(mockedTchapUtils.makeValidatedServerConfig).toHaveBeenCalledWith({
-            base_url: defaultHsUrl,
-            server_name: defaultHsUrl,
-        });
-
-        expect(PlatformPegMocked.startSingleSignOn).toHaveBeenNthCalledWith(
-            1,
-            mockedClient,
-            "sso",
-            "/home",
-            "",
-            SSOAction.LOGIN,
-            userEmail,
-        );
-    });
-
-    it("should start sso with correct homeserver 2", async () => {
-        renderEmailVerificationPage();
-
-        // Mock the implementation without error, what we want is to be sure they are called with the correct parameters
-        mockedFetchHomeserverFromEmail(secondHsUrl);
-        mockedValidatedServerConfig(false, secondHsUrl);
-        mockedPlatformPegStartSSO(false);
-
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: userEmail } });
-
-        await flushPromises();
-
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
-        });
-
-        expect(mockedTchapUtils.makeValidatedServerConfig).toHaveBeenCalledWith({
-            base_url: secondHsUrl,
-            server_name: secondHsUrl,
-        });
-
-        expect(PlatformPegMocked.startSingleSignOn).toHaveBeenNthCalledWith(
-            1,
-            mockedClient,
-            "sso",
-            "/home",
-            "",
-            SSOAction.LOGIN,
-            userEmail,
-        );
-    });
-
-    it("should display error when sso is not configured in homeserer", async () => {
-        const { container } = renderEmailVerificationPage();
-
-        // Mock the implementation without error, what we want is to be sure they are called with the correct parameters
-        mockedFetchHomeserverFromEmail(secondHsUrl);
-        mockedValidatedServerConfig(false, secondHsUrl);
-        mockedPlatformPegStartSSO(false);
-        // get flow without sso configured on homeserver
-        mockedLogin.mockImplementation(() => ({
-            hsUrl: secondHsUrl,
-            createTemporaryClient: jest.fn().mockReturnValue(mockedClient),
-            getFlows: jest.fn().mockResolvedValue([{ type: "m.login.password" }]),
-        }));
-        // Put text in email field
-        const emailField = screen.getByRole("textbox");
-        fireEvent.focus(emailField);
-        fireEvent.change(emailField, { target: { value: userEmail } });
-
-        await flushPromises();
-
-        // click on proconnect button
-        const proconnectButton = screen.getByTestId("proconnect-submit");
-        await act(async () => {
-            await fireEvent.click(proconnectButton);
-        });
-
-        expect(container.getElementsByClassName("mx_ErrorMessage").length).toBe(1);
-    });
-});
     describe("MAS flow activated", () => {
         beforeEach(() => {
-
-            const config: ConfigOptions = 
-            { tchap_mas_flow:{ 
-                isActive: true , 
-                isMASmigration: false
-            } 
+            const config: ConfigOptions = {
+                tchap_mas_flow: {
+                    isActive: true,
+                    isMASmigration: false,
+                },
             };
 
             SdkConfig.put(config);
             // Dans le beforeEach du bloc "MAS flow activated"
-            jest.spyOn(authorize, 'startOidcLogin').mockImplementation(jest.fn());
-
+            jest.spyOn(authorize, "startOidcLogin").mockImplementation(jest.fn());
         });
 
         it("should display correct title and button label when mas flow is activated", () => {
@@ -340,18 +337,17 @@ describe("MAS flow deactivated", () => {
     */
 
     it("should call start oidc native flow with login_hint", async () => {
-        
-        const config: ConfigOptions = 
-            { tchap_mas_flow:{ 
-                isActive: true , 
-                isMASmigration: true 
-            } 
+        const config: ConfigOptions = {
+            tchap_mas_flow: {
+                isActive: true,
+                isMASmigration: true,
+            },
         };
         SdkConfig.put(config);
-        
-         mockedLogin.mockImplementation(() => ({
+
+        mockedLogin.mockImplementation(() => ({
             hsUrl: defaultHsUrl,
-            delegatedAuthentication:{},
+            delegatedAuthentication: {},
             getFlows: jest.fn().mockResolvedValue([{ type: "oidcNativeFlow", clientId: "clientId" }]),
         }));
 
@@ -379,11 +375,9 @@ describe("MAS flow deactivated", () => {
             undefined, // delegatedAuthentication is undefined in this test
             expect.anything(), // clientId
             expect.anything(), // hsUrl
-            expect.anything(), // isUrl 
+            expect.anything(), // isUrl
             expect.anything(), // isRegistration
-            userEmail // loginHint - c'est ce paramètre que nous voulons vérifier
+            userEmail, // loginHint - c'est ce paramètre que nous voulons vérifier
         );
-
     });
-
 });
