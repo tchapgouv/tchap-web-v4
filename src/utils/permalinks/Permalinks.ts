e019faf5261117b461a692d4de2c8f00f51f2a70
marc.sirisak <marc.sirisak@beta.gouv.fr>
2026-02-24 11:19:34 +0100

    TCHAP: add support for new tchap permalinks

--- src/utils/permalinks/Permalinks.ts
+++ src/utils/permalinks/Permalinks.ts
@@ -23,6 +23,8 @@ import SdkConfig from "../../SdkConfig";
 import { ELEMENT_URL_PATTERN } from "../../linkify-matrix";
 import MatrixSchemePermalinkConstructor from "./MatrixSchemePermalinkConstructor";
 
+import TchapToPermalinkConstructor, { baseUrlPattern as tchapToBaseUrlPattern } from "~tchap-web/src/tchap/util/TchapPermalinkConstructor";
+
 // The maximum number of servers to pick when working out which servers
 // to add to permalinks. The servers are appended as ?via=example.org
 const MAX_SERVER_CANDIDATES = 3;
@@ -450,7 +452,13 @@ export function parsePermalink(fullUrl: string): PermalinkParts | null {
         const decodedUrl = decodeURIComponent(fullUrl);
         if (new RegExp(matrixToBaseUrlPattern, "i").test(decodedUrl)) {
             return new MatrixToPermalinkConstructor().parsePermalink(decodedUrl);
-        } else if (fullUrl.startsWith("matrix:")) {
+        } 
+        // :TCHAP: tchapgouv-permalinks . Needs to be put before Elementpermalink check, otherwise it wont enter here
+        else if (new RegExp(tchapToBaseUrlPattern(elementPrefix), "i").test(decodedUrl)) {
+            return new TchapToPermalinkConstructor(elementPrefix).parsePermalink(decodedUrl);
+        } 
+        // end :TCHAP:
+        else if (fullUrl.startsWith("matrix:")) {
             return new MatrixSchemePermalinkConstructor().parsePermalink(fullUrl);
         } else if (elementPrefix && fullUrl.startsWith(elementPrefix)) {
             return new ElementPermalinkConstructor(elementPrefix).parsePermalink(fullUrl);
