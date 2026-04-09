f7c369dad9000ea2dfae979a64a470591b5f8e29
marc.sirisak <marc.sirisak@beta.gouv.fr>
2026-04-09 16:02:40 +0200

    TCHAP: content scanner instead of block, make it retryable

--- src/tchap/components/views/elements/ContentScanningStatus.tsx
+++ src/tchap/components/views/elements/ContentScanningStatus.tsx
@@ -38,7 +38,7 @@ export const ContentScanningStatus: React.FC<ContentScanningStatusProps> = (prop
     if (props.status === "scanning") {
         return (
             <div>
-                <TextWithTooltip class={`mx_ContentScanningStatus mx_ContentScanningStatus_scanning mx_ContentScanningStatus_scanning--${theme}`} tooltip={props.fileName}>
+                <TextWithTooltip className={`mx_ContentScanningStatus mx_ContentScanningStatus_scanning mx_ContentScanningStatus_scanning--${theme}`} tooltip={props.fileName || ""}>
                     {_t("Scanning")}
                 </TextWithTooltip>
             </div>
@@ -48,7 +48,7 @@ export const ContentScanningStatus: React.FC<ContentScanningStatusProps> = (prop
     if (props.status === "unsafe") {
         return (
             <div>
-                <TextWithTooltip class="mx_ContentScanningStatus mx_ContentScanningStatus_unsafe" tooltip={props.fileName}>
+                <TextWithTooltip className="mx_ContentScanningStatus mx_ContentScanningStatus_unsafe" tooltip={props.fileName || ""}>
                     {_t("Content blocked")}
                 </TextWithTooltip>
             </div>
@@ -58,7 +58,7 @@ export const ContentScanningStatus: React.FC<ContentScanningStatusProps> = (prop
     if (props.status === "error") {
         return (
             <div>
-                <TextWithTooltip class="mx_ContentScanningStatus mx_ContentScanningStatus_error" tooltip={props.fileName}>
+                <TextWithTooltip className="mx_ContentScanningStatus mx_ContentScanningStatus_error" tooltip={props.fileName || ""}>
                     {_t("Scan unavailable")}
                 </TextWithTooltip>
             </div>
