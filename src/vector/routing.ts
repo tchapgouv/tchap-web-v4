03ea8299e312b4c8845f5636803b23d12f6925f6
marc.sirisak <marc.sirisak@beta.gouv.fr>
2026-04-22 11:35:44 +0200

    TCHAP: update routing to differenciate login and register

--- src/vector/routing.ts
+++ src/vector/routing.ts
@@ -41,13 +41,17 @@ function onHashChange(): void {
     }
 
     // :TCHAP:
-    //deactivate this check
     // When MAS is activated we don't want to display login or register page
 
-    logger.log("onHashChange MAS active", window.location.hash, lastLocationHashSet);
-    if (["#/login", "#/register"].includes(window.location.hash)) {
+    if (["#/login"].includes(window.location.hash)) {
         logger.log("onHashChange MAS active, replacing with email-precheck-sso");
         window.location.replace("#/email-precheck-sso");
+        routeUrl(window.location);
+        return;
+    }
+    if (["#/register"].includes(window.location.hash)) {
+        logger.log("onHashChange MAS active, replacing with email-precheck-sso?createAccount=true");
+        window.location.replace("#/email-precheck-sso?createAccount=true");
         routeUrl(window.location);
         return;
     }
