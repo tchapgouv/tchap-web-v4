aec1fc9c502f71f9416adf920b7db9f3929bc7f5
Olivier D <odelcroi@gmail.com>
2026-08-14 09:38:02 +0200

    TCHAP: fix docker build after last upgrade (#1657)
    * copy tchap ressources into docker image
    * move files from /lib dockerignored folder

--- apps/web/test/unit-tests/tchap/createRoom-test.ts
+++ apps/web/test/unit-tests/tchap/createRoom-test.ts
@@ -12,7 +12,7 @@ import ExternalAccountHandler from "~tchap-web/src/tchap/ext/ExternalAccountHand
 import SdkConfig, { type ConfigOptions } from "~tchap-web/src/SdkConfig";
 import Modal from "~tchap-web/src/Modal";
 
-jest.mock("~tchap-web/src/tchap/lib/ExternalAccountHandler.ts");
+jest.mock("~tchap-web/src/tchap/ext/ExternalAccountHandler.ts");
 
 describe("createRoom", () => {
     mockPlatformPeg();
